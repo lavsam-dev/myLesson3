3c6b372a2151108a62a53ffcbec8389f04162d9e
lavsam-dev <lavsam@gmail.com>
2021-10-20 23:15:39 +0400

    Выводит в Log, но падает при выводе во view, потому что другой поток

--- app/src/main/java/lavsam/gb/libs/mylesson1/MainPresenter.kt
+++ app/src/main/java/lavsam/gb/libs/mylesson1/MainPresenter.kt
@@ -1,34 +1,87 @@
 package lavsam.gb.libs.mylesson1
 
+import android.util.Log
+import io.reactivex.rxjava3.annotations.NonNull
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.subjects.BehaviorSubject
+import lavsam.gb.libs.mylesson1.MainPresenter.Companion.RX_LABEL
+import java.util.concurrent.TimeUnit
 
 class MainPresenter(private val view: IMainView) {
 
-    val behaviorSubject = BehaviorSubject.create<CounterType>()
+    companion object {
+        const val RX_LABEL = "RX_LABEL"
+    }
 
     private val model = CountersModel()
 
     fun counterClick(type: CounterType) {
 
-        behaviorSubject.onNext(CounterType.COUNTER_OF_DAYS)
+        val behaviorCounter = BehaviorCounter(model)
+        val c1 = Counting(view, behaviorCounter)
+//        behaviorSubject.onNext(CounterType.COUNTER_OF_DAYS)
 
-        val nextValue = when (type) {
+//        val nextValue = when (type) {
+//            CounterType.COUNTER_OF_DAYS -> model.next(0)
+//            CounterType.COUNTER_OF_YEARS -> model.next(1)
+//            CounterType.COUNTER_OF_PAYLOAD -> model.next(2)
+//        }
+//        view.setButtonText(type, nextValue.toString())
+    }
+
+//    init {
+//        behaviorSubject.onNext(CounterType.COUNTER_OF_YEARS)
+//        behaviorSubject.subscribe {
+//            when (it) {
+//                CounterType.COUNTER_OF_DAYS -> TODO()
+//                CounterType.COUNTER_OF_YEARS -> TODO()
+//                CounterType.COUNTER_OF_PAYLOAD -> TODO()
+//            }
+//        }
+//    }
+}
+
+fun BehaviorCounter(model: CountersModel): @NonNull Observable<Int> {
+
+    val list = listOf<CounterType>(CounterType.COUNTER_OF_YEARS, CounterType.COUNTER_OF_YEARS,
+        CounterType.COUNTER_OF_DAYS, CounterType.COUNTER_OF_PAYLOAD)
+    val behaviorInterval = BehaviorSubject.interval(2, TimeUnit.SECONDS)
+    val behaviorJust = BehaviorSubject.fromIterable(list)
+    return BehaviorSubject.zip(behaviorInterval, behaviorJust, {
+            _, t2 ->
+        when (t2) {
             CounterType.COUNTER_OF_DAYS -> model.next(0)
             CounterType.COUNTER_OF_YEARS -> model.next(1)
             CounterType.COUNTER_OF_PAYLOAD -> model.next(2)
         }
-        view.setButtonText(type, nextValue.toString())
+//        view.setButtonText(t2, nextValue.toString())
+    })
+}
+
+class Counting(view: IMainView, val observable: Observable<Int>) {
+    private val observer = object : Observer<Int> {
+        override fun onSubscribe(d: Disposable?) {
+            Log.i(RX_LABEL, "onSubscribe")
+        }
+
+        override fun onNext(t: Int?) {
+            Log.i(RX_LABEL, "onNext ${t.toString()}")
+//            view.setButtonText(CounterType.COUNTER_OF_PAYLOAD, t.toString())
+        }
+
+        override fun onError(e: Throwable?) {
+            Log.i(RX_LABEL, "onError ${e?.message}")
+        }
+
+        override fun onComplete() {
+            Log.i(RX_LABEL, "onComplete")
+        }
     }
 
     init {
-        behaviorSubject.onNext(CounterType.COUNTER_OF_YEARS)
-        behaviorSubject.subscribe {
-            when (it) {
-                CounterType.COUNTER_OF_DAYS -> TODO()
-                CounterType.COUNTER_OF_YEARS -> TODO()
-                CounterType.COUNTER_OF_PAYLOAD -> TODO()
-            }
-        }
+        observable.subscribe(observer)
     }
 }
 
